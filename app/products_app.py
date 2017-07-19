34daa72a1fd2beeeae2b789038a75bd5f4acdcc1
Andrew Gallagher <aeg486@stern.nyu.edu>
2017-07-18 22:20:47 -0400

    new crud project repo

--- app/products_app.py
+++ app/products_app.py
@@ -1,1 +1,14 @@
-print("Hello")
+
+
+print("----------------------------------")
+print("PRODUCTS APPLICATION")
+print("----------------------------------")
+print("Welome @aeg486!")
+print("")
+print("Operation  |  Description")
+print("---------- | ----------------------")
+print(" 'List'    | Display a list of product identifiers and names.")
+print(" 'Show'    | Show information about a product.")
+print(" 'Create'  | Add a new product.")
+print(" 'Update'  | Edit an existing product.")
+print(" 'Destroy' | Delete an existing product.")
